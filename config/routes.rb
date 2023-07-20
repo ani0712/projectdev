17ab74ede8550bad0ec02d662b5bc1d73fdf56b2
Anindita Ballav <aninditaballav44@gmail.com>
2023-07-20 15:59:33 +0530

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "posts#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :posts do
+    resources :comments
+  end
 end
+
