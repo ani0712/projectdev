a5e326549f94cc91792110cf076efeceb700c851
Anindita Ballav <aninditaballav44@gmail.com>
2023-07-28 16:25:17 +0530

    initial commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
     end
   
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :image)
     end
 
     
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+    has_one_attached :image
+    has_many :comments, dependent: :destroy
     validates :title, :content, presence: :true
 end
